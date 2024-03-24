cff208da0c43ba4d1514404ac195bf8e0aa6869b
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-03-24 09:03:11 +0100

    first commit

---
+++ src/main/java/com/emat/multispirng/MultiSpirngApplication.java
@@ -0,0 +1,13 @@
+package com.emat.multispirng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSpirngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiSpirngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emat/multispirng/MultiSpirngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emat.multispirng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiSpirngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
